a4a41c1aecfd79ff59a0dd1aac917c5cfec9dcdb
David Chege <dkc.ushift@gmail.com>
2021-01-09 05:44:30 -0600

    Cleaning up proxy server

--- server.js
+++ server.js
@@ -12,39 +12,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use('/songdata/', createProxyMiddleware({
-  target: 'http://52.37.102.63:3005/',
+  target: 'http://localhost:3005/',
   changeOrigin: true
 }));
 
-app.use('/relatedTracks/', createProxyMiddleware({
-  target: 'http://3.15.220.99:3001/',
-  changeOrigin: true
-}))
 
-app.use('/artistBio/', createProxyMiddleware({
-  target: 'http://34.220.154.45:2000/',
-  changeOrigin: true
-}))
-
-app.use('/songDescription/', createProxyMiddleware({
-  target: 'http://54.191.20.103:2001/',
-  changeOrigin: true
-}))
-
-app.use('/hashtags/', createProxyMiddleware({
-  target: 'http://18.189.26.97:4001/',
-  changeOrigin: true
-}))
-
-app.use('/comments/', createProxyMiddleware({
-  target: 'http://52.14.128.124:4000/',
-  changeOrigin: true
-}))
-
-app.use('/users/', createProxyMiddleware({
-  target: 'http://18.218.58.9:4002/',
-  changeOrigin: true
-}))
 
 app.use('/:current', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/index.html'));
@@ -57,3 +29,34 @@ app.use('/:current', (req, res) => {
 app.listen(port, () => {
   console.log(`Proxy server listening on http://localhost:${port}!`)
 })
+
+//proxy for rest of the services
+// app.use('/relatedTracks/', createProxyMiddleware({
+//   target: 'http://3.15.220.99:3001/',
+//   changeOrigin: true
+// }))
+
+// app.use('/artistBio/', createProxyMiddleware({
+//   target: 'http://34.220.154.45:2000/',
+//   changeOrigin: true
+// }))
+
+// app.use('/songDescription/', createProxyMiddleware({
+//   target: 'http://54.191.20.103:2001/',
+//   changeOrigin: true
+// }))
+
+// app.use('/hashtags/', createProxyMiddleware({
+//   target: 'http://18.189.26.97:4001/',
+//   changeOrigin: true
+// }))
+
+// app.use('/comments/', createProxyMiddleware({
+//   target: 'http://52.14.128.124:4000/',
+//   changeOrigin: true
+// }))
+
+// app.use('/users/', createProxyMiddleware({
+//   target: 'http://18.218.58.9:4002/',
+//   changeOrigin: true
+// }))
